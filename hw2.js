6f0da1dc171b4fc36ac0ca065ce27d822bf1651a
Matvii <matviiostapchyk@gmail.com>
2024-11-18 15:07:50 +0200

    hw2down

--- hw2.js
+++ hw2.js
@@ -1,7 +1,17 @@
-let a = prompt("Ввести число");
-let b = prompt("Ввести степінь");
-let result =1
-for(let i=0;i<b;i++){
-    result *=a
+function pow(){
+    let a = prompt("Ввести число");
+    let b = prompt("Ввести степінь");
+    let result =1
+    if (b<0){
+        for(let i=0;i<-b;i++){
+
+            result /=a
+        }
+    }
+    for(let i=0;i<b;i++){
+
+        result *=a
+    }
+    console.log("Результат: " + +result);
 }
-console.log("Результат: " + result);
+pow()
