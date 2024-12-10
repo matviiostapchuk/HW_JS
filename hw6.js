d0a29c10d76ece621c463ff10562194d17752ed5
Matvii <matviiostapchyk@gmail.com>
2024-12-10 15:50:43 +0200

    in_progress

---
+++ hw6.js
@@ -0,0 +1,58 @@
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ],
+    findValueByKey(companyName){    
+        if(this.name === companyName){
+            //globalInfo = company.clients
+            console.log(globalInfo)
+        }else{
+            console.log(this.name)
+        }
+    }
+    
+};
+
+var globalInfo = company
+var globalInfoHelp = company
+
+
+
+company.findValueByKey("Велика Компанія")
