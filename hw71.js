f2b68625b8d369502c16df5cea38b88bdcc9acc4
Matvii <matviiostapchyk@gmail.com>
2025-01-10 15:06:18 +0200

    hw7

---
+++ hw71.js
@@ -0,0 +1,32 @@
+
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+];
+
+var re = /[a-zA-Z]+\.?[a-zA-Z]*(@gmail|@yahoo).com/; 
+function getInfo(){
+    for(let j=0;j<3;j++){
+        for (let key in arr[j]){
+            if(key=="email"){
+                console.log(arr[j][key] + ': ' + re.test(arr[j][key]));
+            }
+        }
+    }
+}
+getInfo()
+
+
