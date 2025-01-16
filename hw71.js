302a49e2a42201412199370c18f12e9c95b504eb
Matvii <matviiostapchyk@gmail.com>
2025-01-16 15:58:26 +0200

    hw7

--- hw71.js
+++ hw71.js
@@ -16,17 +16,23 @@ var arr = [
         email:"andrii@mail.ru" // Нам такі не підходять
     },
 ];
-
+var a=[];
+let l=0
 var re = /[a-zA-Z]+\.?[a-zA-Z]*(@gmail|@yahoo).com/; 
 function getInfo(){
     for(let j=0;j<3;j++){
         for (let key in arr[j]){
             if(key=="email"&(re.test(arr[j][key]))){
+                
+                a[l]=arr[j][key];
                 console.log(arr[j][key]);
+                l++
             }
         }
     }
 }
 getInfo()
+console.log([a[0],a[1]])
+
 
 
